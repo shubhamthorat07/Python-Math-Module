0327a27e821f68cd37daaeba27298b36c1cbc9fc
Shubham <40167163+shubhamthorat07@users.noreply.github.com>
2020-08-18 20:01:36 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,65 @@
+l = [100,200,300,400,500,600,700]
+print(sum(l))
+
+print(max(l))
+
+print(min(l))
+
+print(l,type(l))
+
+print(id(300))
+
+
+num1 = 255.65
+print(round(num1))
+print(round(num1,1))
+print(round(num1,2))
+
+
+
+
+l1 = [300,500,100,600,800,400,900]
+print(len(l1))
+print(sorted(l1))
+
+
+
+
+
+l2 = [0.1] * 10
+print(l2)
+
+
+
+
+
+import math
+
+num2 = 14.77
+
+print(math.floor(num2), math.ceil(num2))
+
+
+print(math.sqrt(100))
+print(math.factorial(5))
+
+
+print(math.pow(20,2))
+
+
+
+import random
+print(random.random())
+
+
+
+l3 = [1,2,3,4,5,6,7,8,9]
+print(random.choice(l3))
+
+
+print(random.randint(100,200))
+
+
+
+
+
